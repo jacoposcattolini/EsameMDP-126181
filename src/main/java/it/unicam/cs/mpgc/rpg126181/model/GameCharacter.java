4882121b2ed8d9b682c120f3a8241b07b20686df
Jacopo Scattolini <jacopo.scattolini@studenti.unicam.it>
2026-06-19 14:23:16 +0200

    Imposta la struttura della classe GameCharacter

---
+++ src/main/java/it/unicam/cs/mpgc/rpg126181/model/GameCharacter.java
@@ -0,0 +1,33 @@
+package it.unicam.cs.mpgc.rpg126181.model;
+
+public class GameCharacter {
+
+    public static final int HP_PER_LEVEL = 20;
+
+    private final String id;
+    private final String name;
+    private final String power;
+    private final String description;
+    private final String story;
+    private final Ability ability;
+
+    private final int maxHp;
+    private final double damageMultiplier;
+
+    public GameCharacter(String id, String name, String power, String description,
+                         String story, Ability ability, int maxHp, double damageMultiplier) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.description = description;
+        this.story = story;
+        this.ability = ability;
+        this.maxHp = maxHp;
+        this.damageMultiplier = damageMultiplier;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
